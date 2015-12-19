76080a56fa11a3114bd636513ce81adeb9131c85
tongyu <tongyu@tongyu-desktop.(none)>
2015-12-19 23:46:21 +0800

    add the application of Node #1 and Node #2

---
+++ Multi-hops/MultiHops.h
@@ -0,0 +1,31 @@
+#ifndef MULTIHOPS_H
+#define MULTIHOPS_H
+
+static const float SHT11_C1 = -4.0;
+static const float SHT11_C2 = +0.0405;
+static const float SHT11_C3 = -0.0000028;
+static const float SHT11_Temp1 = +0.01;
+static const float SHT11_Temp2 = +0.00008;
+
+enum {
+	AM_MULTIHOPSTORADIO = 6,
+	TIMER_PERIOD_MILLI = 250
+};
+
+typedef nx_struct MultiHopsMsg {
+	nx_uint32_t token;
+	nx_uint16_t nodeid;
+	nx_uint16_t seqnumber;
+	nx_uint16_t temperature;
+	nx_uint16_t humidity;
+	nx_uint16_t light;
+	nx_uint32_t curtime;
+} MultiHopsMsg;
+
+typedef nx_struct ChangeFeqMsg {
+	nx_uint32_t token;
+	nx_uint16_t nodeid;
+	nx_uint32_t newfeq;	
+} ChangeFeqMsg;
+
+#endif
